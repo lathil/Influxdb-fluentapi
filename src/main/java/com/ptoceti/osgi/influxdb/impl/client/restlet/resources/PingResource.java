d5a373efca46121ca56f567730c6f4fc67ee057b
lathik <lathil.dev@gmail.com>
2016-12-08 12:51:49 +0100

    Initial commit

---
+++ src/main/java/com/ptoceti/osgi/influxdb/impl/client/restlet/resources/PingResource.java
@@ -0,0 +1,13 @@
+package com.ptoceti.osgi.influxdb.impl.client.restlet.resources;
+
+import org.restlet.resource.Get;
+import org.restlet.resource.ResourceException;
+
+
+public interface PingResource {
+
+    static String  path = "ping";
+    
+    @Get
+    public void ping();
+}
